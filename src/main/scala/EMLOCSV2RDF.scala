c119bd83a01f3a0506208660065940cab483c9cc
Eetu Mäkelä <eetu.makela@aalto.fi>
2015-09-28 15:24:12 +0300

    Add separate place id property to EMLO places

--- src/main/scala/EMLOCSV2RDF.scala
+++ src/main/scala/EMLOCSV2RDF.scala
@@ -26,19 +26,19 @@ import com.hp.hpl.jena.rdf.model.Property
 import scala.util.Try
 
 object EMLOCSV2RDF extends Anything2RDF {
-  
+
   val sns = "http://emlo.bodleian.ox.ac.uk/schema#"
   val ns = "http://emlo.bodleian.ox.ac.uk/id/"
   val credits = EOP("Credits")
-  
+
   val Letter = EC("Letter")
   Letter.addProperty(RDFS.subClassOf,CIDOC.Linguistic_Object)
-  
+
   val City = EC("City")
   City.addProperty(RDFS.subClassOf, CIDOC.Place)
   val Country = EC("Country")
   Country.addProperty(RDFS.subClassOf, CIDOC.Place)
-  
+
   val Source = EC("Source")
   val Calendar = EC("Calendar")
   val Keyword = EC("Keyword")
@@ -56,7 +56,7 @@ object EMLOCSV2RDF extends Anything2RDF {
   val pddate = EOP("possible death date")
   val addate = EOP("approximate death date")
   val iddate = EOP("inferred death date")
-  
+
   val authors_as_marked = EDP("authors as marked")
   val authors_uncertain = EOP("uncertain author")
   val authors_inferred = EOP("inferred author")
@@ -65,7 +65,7 @@ object EMLOCSV2RDF extends Anything2RDF {
   val addressees_inferred = EOP("inferred addressee")
   val origin_as_marked = EDP("origin as marked")
   val origin_inferred = EDP("inferred origin")
-  val origin_uncertain = EDP("uncertain origin")  
+  val origin_uncertain = EDP("uncertain origin")
   val destination_as_marked = EDP("destination as marked")
   val destination_inferred = EDP("inferred destination")
   val destination_uncertain = EDP("uncertain destination")
@@ -77,7 +77,8 @@ object EMLOCSV2RDF extends Anything2RDF {
   val original_calendar = EOP("original calendar")
 
   val iperson_id = EDP("iperson_id")
-  
+  val place_id = EDP("place_id")
+
   val id_number_or_shelfmark = EDP("id number or shelfmark")
   val printed_edition_details = EDP("printed edition details")
   val paper_size = EDP("paper size")
@@ -97,12 +98,12 @@ object EMLOCSV2RDF extends Anything2RDF {
     val destination_property : Property
   )
 
-  
+
   val unCCRegex = "(?<=[A-Z])(?=[A-Z][a-z])|(?<=[^A-Z])(?=[A-Z])|(?<=[A-Za-z])(?=[^A-Za-z])".r
   def unCamelCase(str : String) : String = {
     unCCRegex.replaceAllIn(str, " ")
   }
-  
+
   def getUUIDURI(id : String)(implicit uuidMap: HashMap[String,String]) : String = {
     ns+uuidMap.getOrElse(id,id)
   }
@@ -136,7 +137,7 @@ object EMLOCSV2RDF extends Anything2RDF {
       if (!w(h("resource_details")).trim.isEmpty) res.addProperty(DCTerms.license,w(h("resource_details")))
 
     }}
-    val manifestationWorkMap = new HashMap[String,String] 
+    val manifestationWorkMap = new HashMap[String,String]
     wr = CSVReader("cofk_union_relationship.csv")
     headers = wr.next
     h = headers.zipWithIndex.toMap
@@ -158,8 +159,8 @@ object EMLOCSV2RDF extends Anything2RDF {
     breakable { for (w <- wr) {
       workNameMap.put(w(h("work_id")),w(h("description")))
       val wo = I(getUUIDURI(w(h("work_id"))),Map("en"->w(h("description"))),Letter)
-      if (!w(h("original_calendar")).trim.isEmpty) wo.addProperty(original_calendar,I(ns+"calendar_"+encode(w(h("original_calendar"))),Map("en"->w(h("original_calendar"))),Calendar)) 
-      if (!w(h("original_catalogue")).trim.isEmpty) wo.addProperty(source,I(ns+"source_"+encode(w(h("original_catalogue"))),w(h("original_catalogue")),Source)) 
+      if (!w(h("original_calendar")).trim.isEmpty) wo.addProperty(original_calendar,I(ns+"calendar_"+encode(w(h("original_calendar"))),Map("en"->w(h("original_calendar"))),Calendar))
+      if (!w(h("original_catalogue")).trim.isEmpty) wo.addProperty(source,I(ns+"source_"+encode(w(h("original_catalogue"))),w(h("original_catalogue")),Source))
       if (!w(h("abstract")).trim.isEmpty) wo.addProperty(DCTerms.description,w(h("abstract")).trim,"en")
       if (!w(h("editors_notes")).trim.isEmpty) wo.addProperty(RDFS.comment,w(h("editors_notes")),"en")
       if (!w(h("incipit")).trim.isEmpty) wo.addProperty(incipit,w(h("incipit")))
@@ -201,40 +202,40 @@ object EMLOCSV2RDF extends Anything2RDF {
 
     }}
 /*
- * csv2rdf Map(addressees_uncertain -> 0, date_of_work_std -> 1779-10-03, date_of_work_inferred -> 0, 
- * date_of_work_std_gregorian -> 1779-10-03, date_of_work_std_year -> 1779, 
- * destination_as_marked -> , editors_notes -> , date_of_work2_std_day -> , date_of_work_approx -> 0, 
- * incipit -> , description -> 3 Oct 1779: Badcock, Samuel, 1747-1788 to Griffiths, Ralph, 1720-1803, 
- * original_catalogue -> Bodleian card catalogue, 
- * authors_inferred -> 0, 
- * date_of_work_uncertain -> 0, 
- * date_of_work_as_marked -> Oct. 3 1779, 
- * addressees_inferred -> 0, destination_inferred -> 0, edit_status -> 
- * date_of_work_std_is_range -> 0, ps -> , 
- * creation_timestamp -> 2010-02-24 00:00:00, date_of_work_std_month -> 10, work_is_translation -> 0, 
- * origin_as_marked -> , keywords -> , 
- * language_of_work -> English, 
- * date_of_work2_std_year -> , 
- * original_calendar -> Unknown, 
- * addressees_as_marked -> Griffiths, R., 
- * explicit -> , 
- * origin_inferred -> 0, authors_uncertain -> 0, 
- * date_of_work_std_day -> 3, 
- * destination_uncertain -> 0, origin_uncertain -> 0, 
+ * csv2rdf Map(addressees_uncertain -> 0, date_of_work_std -> 1779-10-03, date_of_work_inferred -> 0,
+ * date_of_work_std_gregorian -> 1779-10-03, date_of_work_std_year -> 1779,
+ * destination_as_marked -> , editors_notes -> , date_of_work2_std_day -> , date_of_work_approx -> 0,
+ * incipit -> , description -> 3 Oct 1779: Badcock, Samuel, 1747-1788 to Griffiths, Ralph, 1720-1803,
+ * original_catalogue -> Bodleian card catalogue,
+ * authors_inferred -> 0,
+ * date_of_work_uncertain -> 0,
+ * date_of_work_as_marked -> Oct. 3 1779,
+ * addressees_inferred -> 0, destination_inferred -> 0, edit_status ->
+ * date_of_work_std_is_range -> 0, ps -> ,
+ * creation_timestamp -> 2010-02-24 00:00:00, date_of_work_std_month -> 10, work_is_translation -> 0,
+ * origin_as_marked -> , keywords -> ,
+ * language_of_work -> English,
+ * date_of_work2_std_year -> ,
+ * original_calendar -> Unknown,
+ * addressees_as_marked -> Griffiths, R.,
+ * explicit -> ,
+ * origin_inferred -> 0, authors_uncertain -> 0,
+ * date_of_work_std_day -> 3,
+ * destination_uncertain -> 0, origin_uncertain -> 0,
  * date_of_work2_std_month -> , authors_as_marked -> Badcock, S., 1747-1788)
  */
-    
+
 //csv2rdf List(work_id, description, date_of_work_as_marked, original_calendar, date_of_work_std, date_of_work_std_gregorian, date_of_work_std_year, date_of_work_std_month, date_of_work_std_day, date_of_work2_std_year, date_of_work2_std_month, date_of_work2_std_day, date_of_work_std_is_range, date_of_work_inferred, date_of_work_uncertain, date_of_work_approx, authors_as_marked, addressees_as_marked, authors_inferred, authors_uncertain, addressees_inferred, addressees_uncertain, destination_as_marked, origin_as_marked, destination_inferred, destination_uncertain, origin_inferred, origin_uncertain, abstract, keywords, language_of_work, work_is_translation, incipit, explicit, ps, original_catalogue, accession_code, work_to_be_deleted, iwork_id, editors_notes, edit_status, relevant_to_cofk, creation_timestamp, creation_user, change_timestamp, change_user)
     wr = CSVReader("cofk_union_manifestation.csv")
     headers = wr.next
     h = headers.zipWithIndex.toMap
     println(headers.toSeq)
     val manifestationMap = new HashMap[String,Resource]
-    breakable { for (w <- wr) if (!manifestationWorkMap.contains(w(h("manifestation_id")))) logger.warn(s"Manifestation ${w(h("manifestation_id"))} is a manifestation of no work!") else { 
+    breakable { for (w <- wr) if (!manifestationWorkMap.contains(w(h("manifestation_id")))) logger.warn(s"Manifestation ${w(h("manifestation_id"))} is a manifestation of no work!") else {
       val t = EC(w(h("manifestation_type")))
       t.addProperty(RDFS.subClassOf,CIDOC.Information_Carrier)
       val ma = if (w(h("manifestation_type"))=="Letter") {
-        val tmp = m.createResource(getUUIDURI(manifestationWorkMap(w(h("manifestation_id"))))) 
+        val tmp = m.createResource(getUUIDURI(manifestationWorkMap(w(h("manifestation_id")))))
         manifestationMap.put(w(h("manifestation_id")),tmp)
         tmp
       } else I(ns+w(h("manifestation_id")),w(h("manifestation_type"))+" of "+manifestationWorkMap.get(w(h("manifestation_id"))).flatMap(workNameMap.get(_)).getOrElse("?"),t)
@@ -254,7 +255,7 @@ object EMLOCSV2RDF extends Anything2RDF {
       if (!w(h("manifestation_ps")).trim.isEmpty) ma.addProperty(ps,w(h("manifestation_ps")))
       if (w(h("manifestation_is_translation"))=='1') ma.addLiteral(is_translation,true)
       w(h("language_of_manifestation")).split(',').map(_.trim).filter(!_.isEmpty).foreach(k => ma.addProperty(DCTerms.language,I(ns+"language_"+encode(k),Map("en"->k),Language)));
-      if (!w(h("manifestation_creation_calendar")).trim.isEmpty) ma.addProperty(original_calendar,I(ns+"calendar_"+encode(w(h("manifestation_creation_calendar"))),Map("en"->w(h("manifestation_creation_calendar"))),Calendar)) 
+      if (!w(h("manifestation_creation_calendar")).trim.isEmpty) ma.addProperty(original_calendar,I(ns+"calendar_"+encode(w(h("manifestation_creation_calendar"))),Map("en"->w(h("manifestation_creation_calendar"))),Calendar))
       if (!w(h("manifestation_creation_date_year")).trim.isEmpty) {
         val (bdateTime,edateTime) = makeDateTime(w(h("manifestation_creation_date_year")),w(h("manifestation_creation_date_month")),w(h("manifestation_creation_date_day")))
         val date = makeTimeSpan(s"${bdateTime.substring(0,10)}-${edateTime.substring(0,10)}",Some(bdateTime),Some(edateTime),Some(bdateTime),Some(edateTime))
@@ -265,11 +266,11 @@ object EMLOCSV2RDF extends Anything2RDF {
       }
 
     }}
-//csv2rdf List(manifestation_id, manifestation_type, id_number_or_shelfmark, printed_edition_details, paper_size, paper_type_or_watermark, 
-//number_of_pages_of_document, number_of_pages_of_text, seal, postage_marks, endorsements, non_letter_enclosures, 
-//manifestation_creation_calendar, manifestation_creation_date, manifestation_creation_date_gregorian, manifestation_creation_date_year, 
-//manifestation_creation_date_month, manifestation_creation_date_day, manifestation_creation_date_inferred, 
-//manifestation_creation_date_uncertain, manifestation_creation_date_approx, manifestation_is_translation, language_of_manifestation, 
+//csv2rdf List(manifestation_id, manifestation_type, id_number_or_shelfmark, printed_edition_details, paper_size, paper_type_or_watermark,
+//number_of_pages_of_document, number_of_pages_of_text, seal, postage_marks, endorsements, non_letter_enclosures,
+//manifestation_creation_calendar, manifestation_creation_date, manifestation_creation_date_gregorian, manifestation_creation_date_year,
+//manifestation_creation_date_month, manifestation_creation_date_day, manifestation_creation_date_inferred,
+//manifestation_creation_date_uncertain, manifestation_creation_date_approx, manifestation_is_translation, language_of_manifestation,
 //address, manifestation_incipit, manifestation_excipit, manifestation_ps)
     wr = CSVReader("cofk_union_relationship.csv")
     headers = wr.next
@@ -282,7 +283,7 @@ object EMLOCSV2RDF extends Anything2RDF {
           case "cofk_union_relationship_type-was_sent_to" => m.add(m.createResource(getUUIDURI(w(liv))),workMetadataMap.get(w(liv)).map(_.destination_property).getOrElse(m.createProperty(sns+"cofk_union_relationship_type-was_sent_to")),m.createResource(getUUIDURI(w(riv))))
           case "cofk_union_relationship_type-was_addressed_to" => m.add(m.createResource(getUUIDURI(w(liv))),workMetadataMap.get(w(liv)).map(_.addressee_property).getOrElse(m.createProperty(sns+"cofk_union_relationship_type-was_addressed_to")),m.createResource(getUUIDURI(w(riv))))
           case "cofk_union_relationship_type-is_manifestation_of" => if (manifestationMap.get(w(liv)).isEmpty) m.add(m.createResource(getUUIDURI(w(liv))),m.createProperty(sns+w(ind)),m.createResource(getUUIDURI(w(riv))))
-          case _ => 
+          case _ =>
             val liri = manifestationMap.getOrElse(w(liv),imageMap.getOrElse(w(liv), resourceMap.getOrElse(w(liv),m.createResource(getUUIDURI(w(liv))))))
             val riri = manifestationMap.getOrElse(w(liv),imageMap.getOrElse(w(riv), resourceMap.getOrElse(w(riv),m.createResource(getUUIDURI(w(riv))))))
             m.add(liri,m.createProperty(sns+w(ind)),riri)
@@ -298,9 +299,10 @@ object EMLOCSV2RDF extends Anything2RDF {
     val locationFNMap = new HashMap[String,Resource]()
     breakable { for (w <- wr) {
       val l = m.createResource(getUUIDURI(w(h("location_id"))))
+      l.addProperty(place_id,w(h("location_id")).substring(20))
       l.addProperty(RDF.`type`,CIDOC.Place)
       l.addProperty(RDFS.label,w(h("location_name")))
-      w(h("location_synonyms")).split('\n').map(_.trim).filter(!_.isEmpty).foreach{n => 
+      w(h("location_synonyms")).split('\n').map(_.trim).filter(!_.isEmpty).foreach{n =>
         l.addProperty(SKOS.altLabel,n)
         locationMap.put(n,l)
        }
@@ -349,7 +351,7 @@ object EMLOCSV2RDF extends Anything2RDF {
               ret
             }
             else possible(0)
-          
+
         })
         country.addProperty(RDF.`type`,Country)
         city.foreach(c => c.addProperty(CIDOC.place_falls_within,country))
@@ -367,7 +369,7 @@ object EMLOCSV2RDF extends Anything2RDF {
     val personNameMap = new HashMap[String,String]
     breakable { for (w <- wr) {
       val p = I(getUUIDURI(w(h("person_id"))),w(h("foaf_name")),if (!w(h("is_organisation")).trim.isEmpty) CIDOC.Group else CIDOC.Person)
-      personIdMap.put(w(h("iperson_id")),p)      
+      personIdMap.put(w(h("iperson_id")),p)
       personNameMap.put(w(h("iperson_id")),w(h("foaf_name")))
       if (!w(h("gender")).trim.isEmpty) {
         (w(h("gender"))) match {
@@ -380,7 +382,7 @@ object EMLOCSV2RDF extends Anything2RDF {
       if (!w(h("further_reading")).trim.isEmpty) p.addProperty(RDFS.comment,w(h("further_reading")))
       p.addProperty(FOAF.name,w(h("foaf_name")))
       if (!w(h("skos_altlabel")).trim.isEmpty) p.addProperty(SKOS.altLabel,w(h("skos_altlabel")))
-      w(h("skos_hiddenlabel")).split(Array('/',';')).map(_.trim).filter(!_.isEmpty).foreach(p.addProperty(SKOS.hiddenLabel,_))      
+      w(h("skos_hiddenlabel")).split(Array('/',';')).map(_.trim).filter(!_.isEmpty).foreach(p.addProperty(SKOS.hiddenLabel,_))
       w(h("person_aliases")).split(Array('\n',';')).map(_.trim).filter(!_.isEmpty).foreach(p.addProperty(SKOS.altLabel,_))
       if (!w(h("date_of_birth_year")).trim.isEmpty) {
         val (bdateTime,edateTime) = makeDateTime(w(h("date_of_birth_year")),w(h("date_of_birth_month")),w(h("date_of_birth_day")))
@@ -478,7 +480,7 @@ object EMLOCSV2RDF extends Anything2RDF {
       atypeC.addProperty(RDFS.subClassOf,CIDOC.Activity)
       e.addProperty(RDF.`type`,atypeC)
     }
-//id,activity_type_id,activity_name,activity_description,date_type,date_from_year,date_from_month,date_from_day,date_from_uncertainty,date_to_year,date_to_month,date_to_day,date_to_uncertainty,notes_used,additional_notes,creation_timestamp,creation_user,change_timestamp,change_user,event_label    
+//id,activity_type_id,activity_name,activity_description,date_type,date_from_year,date_from_month,date_from_day,date_from_uncertainty,date_to_year,date_to_month,date_to_day,date_to_uncertainty,notes_used,additional_notes,creation_timestamp,creation_user,change_timestamp,change_user,event_label
     wr = CSVReader("pro_location.csv")
     headers = wr.next
     h = headers.zipWithIndex.toMap
@@ -488,13 +490,13 @@ object EMLOCSV2RDF extends Anything2RDF {
       val l = R(getUUIDURI("cofk_union_location-"+w(h("location_id"))))
       e.addProperty(CIDOC.took_place_at,l)
     }
-//id,location_id,change_timestamp,activity_id   
+//id,location_id,change_timestamp,activity_id
     wr = CSVReader("pro_primary_person.csv")
     headers = wr.next
     h = headers.zipWithIndex.toMap
     println(headers.toSeq)
     val activityPrimaryPersonNameMap = new HashMap[String,String]
-    for (w <- wr) 
+    for (w <- wr)
       if (!personNameMap.contains(w(h("person_id")))) logger.warn("Unknown person: "+w.toSeq)
       else {
         val e = R(ns+"activity_"+w(h("activity_id")))
@@ -558,8 +560,8 @@ object EMLOCSV2RDF extends Anything2RDF {
 //id,author,title,chapterArticleTitle,volumeSeriesNumber,issueNumber,pageNumber,editor,
 //placePublication,datePublication,urlResource,abbreviation,fullBibliographicDetails,
 //edition,reprintFacsimile,repository,creation_user,creation_timestamp,change_user,
-//change_timestamp    
-    
+//change_timestamp
+
 //id,subject_id,subject_type,subject_role_id,relationship_id,object_id,
 //object_type,object_role_id,change_timestamp,activity_id
     wr = CSVReader("pro_assertion.csv")
@@ -575,7 +577,7 @@ object EMLOCSV2RDF extends Anything2RDF {
       } else R(ns+"source_"+w(h("source_id")))
       a.addProperty(CIDOC.is_documented_in,s)
     }
-//id,assertion_type,assertion_id,source_id,source_description,change_timestamp    
+//id,assertion_type,assertion_id,source_id,source_description,change_timestamp
     m.setNsPrefixes(PrefixMapping.Standard)
     m.setNsPrefix("org",ORG.ns)
     m.setNsPrefix("dcterms",DCTerms.NS)
