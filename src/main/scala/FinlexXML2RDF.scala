777f3a529d6877151f00c2fd90c687596c793f3f
Eetu Mäkelä <eetu.makela@iki.fi>
2015-10-04 21:14:59 +0300

    gather completer content, change namespaces

--- src/main/scala/FinlexXML2RDF.scala
+++ src/main/scala/FinlexXML2RDF.scala
@@ -21,17 +21,19 @@ import scala.collection.mutable.Stack
 
 object FinlexXML2RDF extends Anything2RDF {
 
-  val sns = "http://ldf.fi/finlex-schema#"
+  val sns = "http://ldf.fi/finlex/schema#"
   val ns = "http://ldf.fi/finlex/"
   
   val Statute = EC("Statute")
   val Part = EC("Part")
   val Chapter = EC("Chapter")
-  val Subheading = EC("Subheadig")
+  val Subheading = EC("Subheading")
   val Section = EC("Section")
+  val Subsection = EC("Subsection")
   val Paragraph = EC("Paragraph")
   val Subparagraph = EC("Subparagraph")
   
+  val completeContent = EDP("Complete content") 
   
   def toId(id : String) : String = {
     id.replaceAll(":", "/")
@@ -48,15 +50,23 @@ object FinlexXML2RDF extends Anything2RDF {
       var nu : String = ""
       var t : String = ""
       var ot : String = ""
+      var ft : String = ""
       val hbuf = new Stack[Resource]
       val otbuf = new Stack[String]
+      val tbuf = new Stack[String]
       otbuf.push(ot)
-      hbuf.push(h)
       while (xml.hasNext) xml.next match {
-         case EvElemStart(_,"sd", attrs, _) => h=I(ns+toId(attrs("id")(0).text),Statute);
+         case EvElemStart(_,"sd", attrs, _) =>
+           hbuf.push(h)
+           tbuf.push(ft)
+           ft = ""
+           h=I(ns+toId(attrs("id")(0).text),Statute);
          case EvElemStart(_,"os", attrs, _) =>
+           tbuf.push(ft)
+           ft = ""
            if(!nu.isEmpty) {
              h.addProperty(RDFS.label,nu,"fi")
+             ft += nu + "\n"
              otbuf.push(ot)
              ot+=", "+nu
              nu=""
@@ -67,8 +77,11 @@ object FinlexXML2RDF extends Anything2RDF {
            c.addProperty(DCTerms.isPartOf,h)
            h=c
          case EvElemStart(_,"lu", attrs, _) =>
+           tbuf.push(ft)
+           ft = ""
            if(!nu.isEmpty) {
              h.addProperty(RDFS.label,nu,"fi")
+             ft += nu + "\n"
              otbuf.push(ot)
              ot+=", "+nu
              nu=""
@@ -79,8 +92,11 @@ object FinlexXML2RDF extends Anything2RDF {
            c.addProperty(DCTerms.isPartOf,h)
            h=c
          case EvElemStart(_,"vo", attrs, _) =>
+           tbuf.push(ft)
+           ft = ""
            if(!nu.isEmpty) {
              h.addProperty(RDFS.label,nu,"fi")
+             ft += nu + "\n"
              otbuf.push(ot)
              ot+=", "+nu
              nu=""
@@ -101,8 +117,11 @@ object FinlexXML2RDF extends Anything2RDF {
                }
              } 
            } else {
+             tbuf.push(ft)
+             ft = ""             
              if(!nu.isEmpty) {
                h.addProperty(RDFS.label,nu,"fi")
+               ft += nu + "\n"
                otbuf.push(ot)
                ot+=", "+nu
                nu=""
@@ -113,8 +132,11 @@ object FinlexXML2RDF extends Anything2RDF {
              h=c
            }
          case EvElemStart(_,"mo", attrs, _) =>
+           tbuf.push(ft)
+           ft = ""
            if(!nu.isEmpty) {
              h.addProperty(RDFS.label,nu,"fi")
+             ft += nu + "\n"
              otbuf.push(ot)
              ot+=", "+nu
              nu=""
@@ -124,7 +146,7 @@ object FinlexXML2RDF extends Anything2RDF {
            otbuf.push(ot)
            val mid = attrs("id")(0).text
            ot+=", "+mid.replaceFirst(".*:M", "")+" mom."
-           c=I(ns+toId(mid),Section)
+           c=I(ns+toId(mid),Subsection)
            c.addProperty(DCTerms.isPartOf,h)
            h=c
          case EvElemStart(_,"ko", attrs, _) =>
@@ -139,6 +161,8 @@ object FinlexXML2RDF extends Anything2RDF {
                }
              } 
            } else {
+             tbuf.push(ft)
+             ft = ""
              hbuf.push(h)
              otbuf.push(ot)
              val mid = attrs("id")(0).text
@@ -159,6 +183,7 @@ object FinlexXML2RDF extends Anything2RDF {
          case EvElemStart(_,"ni", _, _) => 
            ot=xml.next.asInstanceOf[EvText].text
            h.addProperty(RDFS.label,ot,"fi")
+           ft += ot + "\n"
            h.addProperty(SKOS.prefLabel,ot,"fi")
          case EvElemStart(_,"ot", _, _) =>
            var n = xml.next
@@ -166,6 +191,7 @@ object FinlexXML2RDF extends Anything2RDF {
            if (n.isInstanceOf[EvText]) {
              val mot= (if (!nu.isEmpty) nu+": "+n.asInstanceOf[EvText].text else n.asInstanceOf[EvText].text).trim 
              h.addProperty(RDFS.label,mot,"fi")
+             ft += mot + "\n"
              otbuf.push(ot)
              ot+=", "+mot
              h.addProperty(SKOS.prefLabel,ot,"fi")
@@ -185,13 +211,22 @@ object FinlexXML2RDF extends Anything2RDF {
                h.addProperty(RDFS.label,nu,"fi")
                otbuf.push(ot)
                ot+=", "+nu
+               ft+=ot+'\n'
                nu=""
                h.addProperty(SKOS.prefLabel,ot,"fi")
              }           
-             if(!t.trim.isEmpty) h.addProperty(SIOC.content,t.trim,"fi")
+             if(!t.trim.isEmpty) {
+               h.addProperty(SIOC.content,t.trim,"fi")
+               ft+=t.trim+"\n"
+             }
+             if (!ft.trim.isEmpty)
+               h.addProperty(completeContent,ft,"fi")
+             val tmpft = ft
+             ft=tbuf.pop()
+             ft+=tmpft
              t=""
              h=hbuf.pop()
-             ot=otbuf.pop();
+             ot=otbuf.pop()
            case _ => 
          } 
          case _ => 
